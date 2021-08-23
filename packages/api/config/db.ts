c93078b222b143a71b7832e2565dc32916af6ab2
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-08-23 17:14:09 -0400

    Update support for Knex 0.95 version

--- packages/api/config/db.ts
+++ packages/api/config/db.ts
@@ -1,7 +1,8 @@
-import Knex from 'knex'
+import { knex, Knex } from 'knex'
 import knexfile from './knexfile'
 import { APP_ENV } from './env'
 
-const DB_CONN: Knex = Knex(knexfile[APP_ENV]);
+const config: Knex.Config = knexfile[APP_ENV]
+const DB_CONN = knex(config)
 
 export { DB_CONN };
