81f62ebe432917d41aa7afd9c16fd1bad817b72d
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-11-12 14:36:35 -0400

    Save build and prod scripts

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -4,9 +4,9 @@ password="`cat $(dirname $(realpath $0))/password.txt`"
 
 packages_dir="$(dirname $(dirname $(realpath $0)) )/packages"
 
-sshpass -p "$password" scp -P 21098 -r "$packages_dir/admin/build/" villolzq@66.29.132.6:/home/villolzq/approbado-web/packages/admin
+sshpass -p "$password" scp -r "$packages_dir/admin/build/" root@198.199.86.4:/var/www/approbado/packages/admin
 echo "...ADMIN APP DEPLOYED..."
-sshpass -p "$password" scp -P 21098 -r "$packages_dir/app/build/" villolzq@66.29.132.6:/home/villolzq/approbado-web/packages/app
+sshpass -p "$password" scp -r "$packages_dir/app/build/" root@198.199.86.4:/var/www/approbado/packages/app
 echo "...USERS APP DEPLOYED..."
-sshpass -p "$password" scp -P 21098 -r "$packages_dir/frames/build/" villolzq@66.29.132.6:/home/villolzq/approbado-web/packages/frames
+sshpass -p "$password" scp -r "$packages_dir/frames/build/" root@198.199.86.4:/var/www/approbado/packages/frames
 echo "...FRAMES DIRECTORY DEPLOYED..."
