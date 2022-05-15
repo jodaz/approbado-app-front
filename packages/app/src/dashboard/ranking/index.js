3663cd933ad93fd1ab13be99d95ac8bdec1274ff
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-05-15 15:33:23 -0400

    Create search input

--- packages/app/src/dashboard/ranking/index.js
+++ packages/app/src/dashboard/ranking/index.js
@@ -3,6 +3,8 @@ import Box from '@material-ui/core/Box'
 import { axios } from '@approbado/lib/providers'
 import RankingTable from './RankingTable'
 import Spinner from '@approbado/lib/components/Spinner'
+import TextField from '@material-ui/core/TextField'
+import SearchIcon from '@approbado/lib/icons/SearchIcon'
 
 const initialState = {
     data: {},
@@ -10,24 +12,49 @@ const initialState = {
     loaded: false
 }
 
+const initialUrl = `/users?page=0&perPage=5&sort=points&order=desc`
+
 const RankingList = () => {
     const [data, setData] = React.useState(initialState)
+    const [url, setUrl] = React.useState(initialUrl)
 
-    const fetchUsers = React.useCallback(async () => {
-        const { data } = await axios.get('/users?page=0&perPage=5&sort=points&order=desc')
+    const fetchUsers = async () => {
+        const { data } = await axios.get(url)
 
         setData({...data, loaded: true })
-    }, [])
+    }
+
+    const handleOnChange = (e) => {
+        if (e.currentTarget.value) {
+            setUrl(`${initialUrl}+&filter%5Bglobal_search%5D=${e.currentTarget.value}`)
+        } else {
+            setUrl(initialUrl)
+        }
+    }
 
     React.useEffect(() => {
         fetchUsers();
-    }, [])
+    }, [url])
 
     return (
         <Box display='flex' flexDirection='column'>
+            <TextField
+                onChange={handleOnChange}
+                InputProps={{
+                    startAdornment: (
+                        <Box marginLeft='6px' display='flex'>
+                            <SearchIcon />
+                        </Box>
+                    )
+                }}
+            />
             {data.loaded ? <RankingTable {...data} /> : <Spinner />}
         </Box>
     )
+}
+
+RankingList.defaultValues = {
+    search: ''
 }
 
 export default RankingList
