fdbb399d328664ad78926ded74a94dc0a5d9bf56
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-11-16 16:35:50 -0400

    fix validations on forums

--- packages/lib/layouts/forums/validate.js
+++ packages/lib/layouts/forums/validate.js
@@ -7,6 +7,9 @@ const validateForum = values => {
     if (!values.categories_ids) {
         errors.categories_ids = 'Seleccione al menos una categoría.'
     } else {
+        if (!values.categories_ids.length) {
+            errors.categories_ids = 'Seleccione una o más etiquetas.'
+        }
         if (values.categories_ids.length > 3) {
             errors.categories_ids = 'Máximo tres temas.'
         }
@@ -14,6 +17,9 @@ const validateForum = values => {
     if (!values.trivias_ids) {
         errors.trivias_ids = 'Seleccione al menos una trivia.'
     } else {
+        if (!values.trivias_ids.length) {
+            errors.trivias_ids = 'Seleccione uno o más temas.'
+        }
         if (values.trivias_ids.length > 3) {
             errors.trivias_ids = 'Máximo tres temas.'
         }
