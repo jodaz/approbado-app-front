9086db2124a0a17540edf4989a405d1161fb0b78
Jesus Ordosgoitty <jesuodz@gmail.com>
2023-10-18 16:58:55 -0400

    remove web-env temporally

--- packages/lib/env/index.js
+++ packages/lib/env/index.js
@@ -1,12 +1,5 @@
 import { NATIVE_ENV } from './native-env'
 
-const dinamicallyLoadWebEnv = async () => {
-    const module = await import('./web-env');
-
-    return module.default;
-};
-
-// const CONFIG_NAMES = process.env.EXPO_PUBLIC_API_DOMAIN ? NATIVE_ENV : dinamicallyLoadWebEnv();
 CONFIG_NAMES = NATIVE_ENV;
 
 export default CONFIG_NAMES;
