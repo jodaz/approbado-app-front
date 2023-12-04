4755c9c8fa1f4f6e71b3bb14694b79fb6cd814a9
Jesus Ordosgoitty <jesuodz@gmail.com>
2023-12-03 21:33:25 -0400

    create bottom drawer navigation

---
+++ packages/mobile/components/BottomDrawer.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { Modal, TouchableOpacity, Dimensions } from 'react-native';
+import { IComp } from '../types';
+import styled from 'styled-components/native';
+import Row from './Row';
+
+const { height, width } = Dimensions.get('screen')
+
+const Content = styled.View`
+    position: absolute;
+    left: 0;
+    right: 0;
+    flex-direction: column;
+    height: fit-content;
+    background-color: white;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    padding: 23px;
+    bottom: 0;
+    border-width: 1px;
+    border-color: ${props => props.theme.palette.secondary.light};
+    elevation: 5;
+`
+
+interface IBottomDrawerProps extends IComp {
+    isOpen: boolean;
+    handleClose: () => void;
+}
+
+const BottomDrawer = ({ isOpen, handleClose, children } : IBottomDrawerProps) => (
+    <Modal
+        animationType="fade"
+        transparent
+        visible={isOpen}
+        onRequestClose={handleClose}
+    >
+        <TouchableOpacity style={{
+            backgroundColor: 'rgba(52, 52, 52, 0.8)',
+            height: height,
+            width: width
+        }} onPress={handleClose}/>
+        <Content>
+            {React.Children.map(children, (child) => (
+                <Row>
+                    {React.cloneElement(child)}
+                </Row>
+            ))}
+        </Content>
+    </Modal>
+)
+
+export default BottomDrawer
--- packages/mobile/screens/Home/index.tsx
+++ packages/mobile/screens/Home/index.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react'
 import { useForm } from 'react-hook-form';
 import Container from '../../components/Container';
-import Text from '../../components/Text';
-import { Button, Row } from '../../components';
+import { Text, Button, Row } from '../../components';
 import { Routes } from '../routes';
 import { Mail } from 'lucide-react-native';
 import Logotipo from '@approbado/lib/illustrations/Logotipo.svg'
+import BottomDrawer from '../../components/BottomDrawer';
 
 const Home = ({ navigation }) => {
     const { control, handleSubmit } = useForm();
+
+    // This state would determine if the drawer sheet is visible or not
+    const [isBottomSheetOpen, setIsBottomSheetOpen] = React.useState(false);
+
+    // Function to open the bottom sheet
+    const handleOpenBottomSheet = () => {
+      setIsBottomSheetOpen(true);
+    };
+
+    // Function to close the bottom sheet
+    const handleCloseBottomSheet = () => {
+      setIsBottomSheetOpen(false);
+    };
 
     const onSubmit = async (values) => {
     };
@@ -20,7 +33,21 @@ const Home = ({ navigation }) => {
                 <Button variant='text' onPress={() => navigation.navigate(Routes.Chat)}>
                     <Mail size={24} color='#000' />
                 </Button>
+                <Button variant="outlined" onPress={handleOpenBottomSheet}>
+                    Abrir
+                </Button>
             </Row>
+            <BottomDrawer
+                isOpen={isBottomSheetOpen}
+                handleClose={handleCloseBottomSheet}
+            >
+                <Button variant='text' onPress={() => {
+                    console.log("Hola")
+                    handleCloseBottomSheet()
+                }}>
+                    Cerrar
+                </Button>
+            </BottomDrawer>
         </Container>
     );
 }
