d8337bee496e917f548a3540bf4f211e076aa83e
Jesus Ordosgoitty <jesuodz@gmail.com>
2023-12-08 00:29:05 -0400

    fix chat cards

--- packages/mobile/screens/Chat/components/ChatCard.tsx
+++ packages/mobile/screens/Chat/components/ChatCard.tsx
@@ -6,44 +6,72 @@ import { Image, Text, Row } from '../../../components';
 import format from 'date-fns/format';
 import truncateString from '@approbado/lib/utils/truncateString'
 import styled from 'styled-components/native';
+import { View } from 'react-native';
+import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict';
+import { es } from 'date-fns/locale'
 
 const Pressable = styled.Pressable`
     display: flex;
     justify-content: space-between;
     flex-direction: row;
+    padding: ${props => props.theme.space[2]};
+    margin-top: ${props => props.theme.space[1]};
+    margin-bottom: ${props => props.theme.space[1]};
     width: 100%;
-    height: fit-content;
 `
 
 const ChatCard = ({ item } : Chat ) : JSX.Element => {
     const navigation = useNavigation();
-    const messageTime = format(new Date(message.created_at), 'h:mm a');
+    const lastMessage = item.messages.length
+        ? item.messages[0]
+        : null;
+    const [datetime, setDatetime] = React.useState(null)
 
     const handleNavigate = () => navigation.navigate(Routes.UserChat, {
         chat: item
     })
 
+    const getDistanceInWords = () => formatDistanceToNowStrict(new Date(lastMessage?.created_at), {
+        locale: es,
+    }).slice(0, 12)
+
+    React.useEffect(() => {
+        if (lastMessage?.created_at) {
+            setDatetime(getDistanceInWords())
+
+            const interval = setInterval(() => {
+                setDatetime(getDistanceInWords())
+            }, 5000);
+
+            return () => {
+                clearInterval(interval);
+            };
+        }
+    }, [])
+
     return (
         <Pressable onPress={handleNavigate} key={item.id}>
-            <Row size={3} direction='row' align='start'>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
                 <Image source={item.participants[1].picture} />
-
-                <Text fontSize={18}>
-                    {item.participants[1].user_name}
-                </Text>
-                <Row size={2} align='start'>
+                <View style={{ flex: 1, flexDirection: 'column' }}>
+                    <Text fontSize={18}>
+                        {item.participants[1].user_name}
+                    </Text>
                     <Text
                         fontSize={18}
                         color="secondary"
                         fontWeight={400}
                     >
-                        {item.messages?.length
-                            ? truncateString(item.messages[0].message, 20)
+                        {lastMessage
+                            ? truncateString(lastMessage.message, 20)
                             : null
                         }
                     </Text>
-                </Row>
-            </Row>
+                </View>
+                <Text fontSize={16} fontWeight={400}>
+                    {datetime}
+                </Text>
+            </View>
         </Pressable>
     )
 }
