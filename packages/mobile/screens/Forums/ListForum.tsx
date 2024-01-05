d06f5748e85574d869c1458e573ad7b6c16dde0a
Jesus Ordosgoitty <jesuodz@gmail.com>
2024-01-04 23:27:18 -0400

    create post warning

--- packages/mobile/screens/Forums/ListForum.tsx
+++ packages/mobile/screens/Forums/ListForum.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Container } from '../../components';
-import { FloatingButton } from '../../components';
-import { Plus } from 'lucide-react-native';
-import { Routes } from '../routes';
 import Tabs from '../../components/Tabs';
 import PopularPosts from './components/PopularPosts';
 import UnansweredPosts from './components/UnansweredPosts';
 import NewPosts from './components/NewPosts';
 import ForumSearchbox from './components/ForumSearchbox';
+import CreatePostWarning from './components/CreatePostWarning';
 
 const screens = [
     {
@@ -43,10 +41,7 @@ const ListPosts = ({ navigation }) => {
             <Tab.Navigator initialRouteName="New" tabBar={Tabs}>
                 {screens.map(screen => <Tab.Screen {...screen} />)}
             </Tab.Navigator>
-            <FloatingButton
-                icon={<Plus />}
-                onPress={() => navigation.navigate(Routes.CreateForum)}
-            />
+            <CreatePostWarning />
         </Container>
     );
 }
